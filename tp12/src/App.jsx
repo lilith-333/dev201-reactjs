2d1bef9a873c3abac135bc3c5c08fcee5c86a065
=profnadir <=hamzanadir.dev@gmail.com>
2024-12-27 15:00:54 +0100

    tp12

---
+++ tp12/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Form from './Form'
+import ListArticle from './ListArticle'
+
+function App() {
+
+  return (
+    <>
+      {/* <Form /> */}
+      <ListArticle/>
+    </>
+  )
+}
+
+export default App
